b66f54318b2fcc4abdb35c084e554bd4e11eb218
Federico Gobea <fgobea@making.com.ar>
2021-05-05 14:37:29 -0300

    [INIT] enviopack: module init

---
+++ stock_enviopack/__manifest__.py
@@ -0,0 +1,22 @@
+{
+    'name': 'Envio Pack Integration',
+    'version': "13.0.1.2.0",
+    'category': 'Warehouse Management',
+    'sequence': 14,
+    'summary': '',
+    'author': 'Making Argentina',
+    'website': 'www.making.com.ar',
+    'license': 'AGPL-3',
+    'images': [
+    ],
+    'depends': [
+    ],
+    'data': [
+      
+    ],
+    'demo': [
+    ],
+    'installable': True,
+    'auto_install': False,
+    'application': False,
+}
