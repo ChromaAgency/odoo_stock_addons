8224e8f5316f570497ad8166e0f224747fc00e2e
Federico Gobea <fgobea@making.com.ar>
2021-06-25 10:55:22 -0300

    [add] sale.order: virtual available on sale.order.line form view

--- stock_sale_ux/models/__init__.py
+++ stock_sale_ux/models/__init__.py
@@ -4,5 +4,6 @@ from odoo.fields import Float
 class SaleOrderLine(Model):
     _inherit = 'sale.order.line'
 
-    qty_available = Float(string="Cantidad en inventario", related="product_id.qty_available")
+    qty_available = Float(string="Stock a mano", related="product_id.qty_available")
+    virtual_available = Float(string="Stock previsto", related="product_id.virtual_available")
     
