bb63e00c2bf1b65ca2efd20386170f7296111226
Juan Pablo Castiglione <juanpablo@dicromingenieria.com.ar>
2021-10-26 17:08:17 -0300

    fixes

---
+++ l10n_ar_stock/__manifest__.py
@@ -0,0 +1,33 @@
+{
+    'name': ' stock para Argentina',
+    'version': '14.0.1.15.0',
+    'category': 'Localization/Argentina',
+    'sequence': 14,
+    'author': 'Making SA',
+    'website': 'www.making.com.ar',
+    'license': 'AGPL-3',
+    'depends': [
+        'stock_voucher',  # por la clase receiptbooks y demas
+        'l10n_ar',  
+    ],
+    'data': [
+        'security/l10n_ar_stock_security.xml',
+        'wizards/arba_cot_wizard_views.xml',
+        'wizards/res_config_settings_view.xml',
+        'views/stock_picking_views.xml',
+        'views/stock_book_views.xml',
+        'views/product_template_views.xml',
+        'views/uom_uom_views.xml',
+        'views/stock_production_lot_views.xml',
+        'views/report_deliveryslip.xml',
+        'views/report_invoice.xml',
+        'data/ir_sequence_data.xml',
+        'data/product_uom_data.xml',
+        'data/document_type_data.xml',
+    ],
+    'demo': [
+    ],
+    'installable': True,
+    'auto_install': False,
+    'application': False,
+}
---
+++ l10n_ar_stock_delivery/__manifest__.py
@@ -0,0 +1,21 @@
+{
+    'name': 'Integracion entre modulo delivery y localización argentina',
+    'version': '14.0.1.4.0',
+    'category': 'Localization/Argentina',
+    'sequence': 14,
+    'author': 'Making SA',
+    'website': 'www.making.com.ar',
+    'license': 'AGPL-3',
+    'depends': [
+        'delivery_ux',
+        'l10n_ar_stock',
+    ],
+    'data': [
+        'views/report_deliveryslip.xml',
+    ],
+    'demo': [
+    ],
+    'installable': True,
+    'auto_install': True,
+    'application': False,
+}
