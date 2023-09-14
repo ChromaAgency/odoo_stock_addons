ab4906d67b6974f348134e24cf2b3da112c9a90c
Federico Gobea <127751411+making-fgobea@users.noreply.github.com>
2023-09-14 12:56:34 -0500

    ✨ add newmodule that replaces name on report with correct company

---
+++ l10n_ar_stock_accounting_separated_company/__manifest__.py
@@ -0,0 +1,20 @@
+{
+    'name': ' stock para Argentina',
+    'version': '0.1',
+    'category': 'Localization/Argentina',
+    'sequence': 14,
+    'author': 'Chroma',
+    'website': 'www.chroma.agency',
+    'license': 'AGPL-3',
+    'depends': [
+        'l10n_ar_stock', 'accounting_separated_company'
+    ],
+    'data': [
+      'reports/custom_header.xml',
+    ],
+    'demo': [
+    ],
+    'installable': True,
+    'auto_install': False,
+    'application': False,
+}
