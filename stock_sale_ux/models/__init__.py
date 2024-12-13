f4524dce48206624430a40a119fb6d2c177524a8
MRIVERA <mrivera@chroma.agency>
2024-12-13 16:18:01 -0300

    ⬆️

--- stock_sale_ux/models/__init__.py
+++ stock_sale_ux/models/__init__.py
@@ -1,9 +1,1 @@
-from odoo.models import Model
-from odoo.fields import Float
-
-class SaleOrderLine(Model):
-    _inherit = 'sale.order.line'
-
-    qty_available = Float(string="Stock a mano", related="product_id.qty_available")
-    virtual_available = Float(string="Stock previsto", related="product_id.virtual_available")
-    
+from . import sale_order_line
