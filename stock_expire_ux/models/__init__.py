cb24780f01a80d765d775bf83dee43983bf7526f
MRIVERA <mrivera@chroma.agency>
2024-12-13 11:31:24 -0300

    🎨 improved structure of code but is working

--- stock_expire_ux/models/__init__.py
+++ stock_expire_ux/models/__init__.py
@@ -1,7 +1,1 @@
-from odoo.models import Model
-from odoo.fields import Datetime
-
-class StockMoveLine(Model):
-    _inherit = 'stock.move.line'
-
-    use_date = Datetime(related="lot_id.use_date", readonly=False)
+from . import stock_move_line
